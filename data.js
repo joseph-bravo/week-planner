fb6b0f67692877ad8286ca00e7dce41e4d3a7b0a
Nick Newton <nickanewton@gmail.com>
2022-04-08 19:58:21 +0000

    saving progress.

---
+++ data.js
@@ -0,0 +1,8 @@
+/* exported data */
+var data = {
+  nextEntryID: 1,
+  entries: [],
+  view: 'entries',
+  viewWeekDay: 'sun',
+  editing: null
+};
--- main.js
+++ main.js
@@ -1,2 +1,21 @@
+/* global data */
 var $entryForm = document.querySelector('form#entry-form');
 var $entryWeekday = document.querySelector('#weekday');
+var $entryTime = document.querySelector('#time');
+var $entryDescription = document.querySelector('#description');
+var $entryBtn = document.querySelector('#submit-btn');
+
+function submitHandler(event) {
+  event.preventDefault();
+
+  var entryObj = {
+    weekday: $entryWeekday.value,
+    time: $entryTime.value,
+    description: $entryDescription.value,
+    entryId: data.nextEntryID
+  };
+
+  console.log('entry obj', entryObj);
+}
+
+$entryForm.addEventListener('submit', submitHandler);
